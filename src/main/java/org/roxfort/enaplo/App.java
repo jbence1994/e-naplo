2c69ca44d4407edc9e29fd4ba8b91205092df988
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2020-11-25 15:42:36 +0100

    add font.

--- src/main/java/org/roxfort/enaplo/App.java
+++ src/main/java/org/roxfort/enaplo/App.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     public void start(Stage primaryStage) throws Exception {
+        Font.loadFont(getClass().getResource("/fonts/VarelaRound-Regular.ttf")
+                .toExternalForm(), 10);
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
 
         primaryStage.setTitle("e-napló | Roxfort");
